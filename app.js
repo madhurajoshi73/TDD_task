69a6bf118845fd79767115f510b6f30f34c91e3b
madhurajoshi73 <joshimadhura73@gmail.com>
2024-09-17 15:24:02 +0530

    more test case with empty string and custom delimitter

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 export function add(numbers){
-    var arr = numbers.split(/[,\n]+/).filter(a=>a!='')
+    var arr = numbers.split(/[,\n]+/).filter(a=>a!='' && a!=" ")
     var negativenum=[]
     try{
     if(arr.length!=0 && arr[0].startsWith("//")){
-        arr.filter(a=>!a.startsWith("//"))
-        var delim = arr[0].substr(2)
-        arr=arr[1].split(delim)
+        var temp =  numbers.split(/\n/).filter(a=>a!='')
+        var delim = temp[0].substr(2)
+        var filttemp = temp.filter(a=>!a.startsWith("//")).join("\n")
+        var re = new RegExp("[\n," + delim + "]+", "g");
+        arr=filttemp.split(re).filter(a=>a!='' && a!=" ")
     }
     var add=0
     if(numbers==0 || arr.length==0){
--- test/test.js
+++ test/test.js
@@ -9,8 +9,16 @@ describe('Math Functions', () => {
     const result = add("1,2,3,4,5,6");
     expect(result).to.equal(21);
   });
+  it('should add many numbers with space', () => {
+    const result = add("1, ,3,4,5,6");
+    expect(result).to.equal(19);
+  });
   it('empty string', () => {
     const result = add("");
+    expect(result).to.equal(0);
+  });
+  it('empty string with space', () => {
+    const result = add(" ");
     expect(result).to.equal(0);
   });
   it('one number string', () => {
@@ -29,9 +37,33 @@ describe('Math Functions', () => {
     const result = add("//;\n1;2;3");
     expect(result).to.equal(6);
   });
+  it('string with custom limiter & with \\n', () => {
+    const result = add("//;\n1;2\n3");
+    expect(result).to.equal(6);
+  });
+  it('string with custom limiter & with \\n and ","', () => {
+    const result = add("//;\n1;2\n3,4");
+    expect(result).to.equal(10);
+  });
+  it('empty string with custom limiter', () => {
+    const result = add("//;\n ; ");
+    expect(result).to.equal(0);
+  });
+  it('empty string with custom limiter & with \\n and ","', () => {
+    const result = add("//;\n,;");
+    expect(result).to.equal(0);
+  });
+  it('empty string with custom limiter & with \\n and "," with space', () => {
+    const result = add("//;\n, ;");
+    expect(result).to.equal(0);
+  });
   it('should add many numbers', () => {
     const result = add("//;\n1;2;3;4;5;6");
     expect(result).to.equal(21);
+  });
+  it('should add many numbers with space', () => {
+    const result = add("//;\n1; ;3;4;5;6");
+    expect(result).to.equal(19);
   });
   it('string with negative number', () => {
     const result = add("-1,2,-3");
@@ -40,5 +72,21 @@ describe('Math Functions', () => {
   it('string with negative number and custom limiter', () => {
     const result = add("//;\n1;2;-3");
     expect(result).to.equal("Negative numbers not allowed -3")
+  });
+  it('string with negative number and custom limiter with space', () => {
+    const result = add("//;\n1; ;-3");
+    expect(result).to.equal("Negative numbers not allowed -3")
+  });
+  it('string with negative number and custom limiter & \\n with space', () => {
+    const result = add("//;\n1\n ;-3");
+    expect(result).to.equal("Negative numbers not allowed -3")
+  });
+  it('string with negative number and custom limiter & "," with space', () => {
+    const result = add("//;\n1, ;-3");
+    expect(result).to.equal("Negative numbers not allowed -3")
+  });
+  it('empty string with custom limiter & with \\n and ","', () => {
+    const result = add("//;\n , ");
+    expect(result).to.equal(0);
   });
 })
